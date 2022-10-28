ac70ecd90e68a7cf4d689c84a510e24afcf00730
Mehmet Kaplan <mehmet.kaplan@gmail.com>
2022-10-28 21:45:46 +0300

    loginuserviatoken frontend tests are completed

--- frontend/__tests__/serial-entrepreneur-frontend-handlers.test.js
+++ frontend/__tests__/serial-entrepreneur-frontend-handlers.test.js
@@ -2,19 +2,29 @@
 // CHECK-BEFORE-BUILD 
 // Obtain following users from backend jest tests
 const testUsers = [
-	{"name":"1666980976477-0","email":"1666980976477-0@yopmail.com","password":"1666980976477-0","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjI5NiwiZW1haWwiOiIxNjY2OTgwOTc2NDc3LTBAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODA5NzcsImV4cCI6MTY2OTY1OTM3N30.MnfZ4Cm5BH6Fv9tdWTA8AxvtDoU0mHjb5JjTfc8W_yQ"},
-	{"name":"1666980976477-1","email":"1666980976477-1@yopmail.com","password":"1666980976477-1","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjI5NywiZW1haWwiOiIxNjY2OTgwOTc2NDc3LTFAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODA5NzgsImV4cCI6MTY2OTY1OTM3OH0.8RiRUkqC_R9uuK5KQb56wBn2MgebsOy7Zk6nh0DsIqk"},
-	{"name":"1666980976477-2","email":"1666980976477-2@yopmail.com","password":"1666980976477-2","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjI5OCwiZW1haWwiOiIxNjY2OTgwOTc2NDc3LTJAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODA5NzksImV4cCI6MTY2OTY1OTM3OX0.a7NnFSo77q15-Cnbwqy8qcc97JwDTr0d5qrm05N8V3w"},
-	{"name":"1666980976477-3","email":"1666980976477-3@yopmail.com","password":"1666980976477-3","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjI5OSwiZW1haWwiOiIxNjY2OTgwOTc2NDc3LTNAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODA5ODAsImV4cCI6MTY2OTY1OTM4MH0.LZAliTqLoYO4V78wGmeOLhA4_ZHuau1WvPFcuQjw6mE"},
-	{"name":"1666980976477-4","email":"1666980976477-4@yopmail.com","password":"1666980976477-4","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMwMCwiZW1haWwiOiIxNjY2OTgwOTc2NDc3LTRAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODA5ODEsImV4cCI6MTY2OTY1OTM4MX0.m-lFED54pv_Gz9JhLF4TG7SUKo4Y2V5iKYhvJYV54EA"},
-	{"name":"1666980976477-5","email":"1666980976477-5@yopmail.com","password":"1666980976477-5","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMwMSwiZW1haWwiOiIxNjY2OTgwOTc2NDc3LTVAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODA5ODEsImV4cCI6MTY2OTY1OTM4MX0.aaXDZGCAmcTi3BrH7VT9kWzY7qtfzXGRz731knHbqog"},
-	{"name":"1666980976477-6","email":"1666980976477-6@yopmail.com","password":"1666980976477-6","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMwMiwiZW1haWwiOiIxNjY2OTgwOTc2NDc3LTZAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODA5ODIsImV4cCI6MTY2OTY1OTM4Mn0.6fPOExGKbG4fC5LAy-SMgM5ZAdtjsWxVV26GS0Hbcdo"},
-	{"name":"1666980976477-7","email":"1666980976477-7@yopmail.com","password":"1666980976477-7","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMwMywiZW1haWwiOiIxNjY2OTgwOTc2NDc3LTdAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODA5ODMsImV4cCI6MTY2OTY1OTM4M30.aVzaYpXxYQZFrhEoQ39WUgWIFATbNvMGe5GX-Nc8G68"},
-	{"name":"1666980976477-8","email":"1666980976477-8@yopmail.com","password":"1666980976477-8","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMwNCwiZW1haWwiOiIxNjY2OTgwOTc2NDc3LThAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODA5ODQsImV4cCI6MTY2OTY1OTM4NH0.4MDN5D1YIm4qGFYVV8p13F95HzYcy-SczUy_vqJT5jI"},
-	{"name":"1666980976477-9","email":"1666980976477-9@yopmail.com","password":"1666980976477-9","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMwNSwiZW1haWwiOiIxNjY2OTgwOTc2NDc3LTlAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODA5ODUsImV4cCI6MTY2OTY1OTM4NX0.l01N1T4lMWFLWdEeaUx_Mu8Xw0uwwI9yUKDDcIYt4RA"},	
+	{"name":"1666982550049-0","email":"1666982550049-0@yopmail.com","password":"1666982550049-0","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMxOCwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTBAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODI1NTAsImV4cCI6MTY2OTY2MDk1MH0.-WmCARmCmYTVyqn4lRG5yDRZpvzJvqJfrfVCK9FmjQ8"},
+	{"name":"1666982550049-1","email":"1666982550049-1@yopmail.com","password":"1666982550049-1","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMxOSwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTFAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODI1NTEsImV4cCI6MTY2OTY2MDk1MX0.2ZakL-E036y7jYrmDjS5033-gnGK2OAg6D8LAd8YBVs"},
+	{"name":"1666982550049-2","email":"1666982550049-2@yopmail.com","password":"1666982550049-2","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMyMCwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTJAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODI1NTIsImV4cCI6MTY2OTY2MDk1Mn0.2YL-bNHGzG3b3z1hZj4wr1WWfLrvdx-it79kBVx2Kg4"},
+	{"name":"1666982550049-3","email":"1666982550049-3@yopmail.com","password":"1666982550049-3","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMyMSwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTNAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODI1NTMsImV4cCI6MTY2OTY2MDk1M30.PNo1UhsnDvOiMi3_R9yT0YptIMU_0ztpFXrub2w7Rqg"},
+	{"name":"1666982550049-4","email":"1666982550049-4@yopmail.com","password":"1666982550049-4","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMyMiwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTRAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODI1NTQsImV4cCI6MTY2OTY2MDk1NH0.rA0--5q57ky3sDM0Uz0Elxos93eBuHr3oqwToPAUuhQ"},
+	{"name":"1666982550049-5","email":"1666982550049-5@yopmail.com","password":"1666982550049-5","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMyMywiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTVAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODI1NTUsImV4cCI6MTY2OTY2MDk1NX0.vxUdSNMkYKhmGXi4qgF1316Wgexe9xBVhUX--kmt-DE"},
+	{"name":"1666982550049-6","email":"1666982550049-6@yopmail.com","password":"1666982550049-6","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMyNCwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTZAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODI1NTYsImV4cCI6MTY2OTY2MDk1Nn0.EjeIwrQdGsZPAq4bObX81GrKR7PFaDWmvFCz_nQcsQQ"},
+	{"name":"1666982550049-7","email":"1666982550049-7@yopmail.com","password":"1666982550049-7","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMyNSwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTdAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODI1NTcsImV4cCI6MTY2OTY2MDk1N30.TB_aIJiPk_FHVU0D3FPEFryE25lL_S6slqS1EyMT20A"},
+	{"name":"1666982550049-8","email":"1666982550049-8@yopmail.com","password":"1666982550049-8","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMyNiwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LThAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODI1NTgsImV4cCI6MTY2OTY2MDk1OH0.-tMVrUbYsYucEv8nMYvrGlOLNjmeBxiqAi4Op2anZZw"},
+	{"name":"1666982550049-9","email":"1666982550049-9@yopmail.com","password":"1666982550049-9","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMyNywiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTlAeW9wbWFpbC5jb20iLCJpYXQiOjE2NjY5ODI1NTksImV4cCI6MTY2OTY2MDk1OX0.53MKgtEcm3c6ic_RIqCpdSmjIwnrhyj3vDKlctlYs7c"},
+	{"name":"1666982550049-10","email":"1666982550049-10@yopmail.com","password":"1666982550049-10","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMyOCwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTEwQHlvcG1haWwuY29tIiwiaWF0IjoxNjY2OTgyNTYwLCJleHAiOjE2Njk2NjA5NjB9.A36yzE0oi8ZQ_RP8Id8THf3Mk8n5vIhio9OM3oBDOk8"},
+	{"name":"1666982550049-11","email":"1666982550049-11@yopmail.com","password":"1666982550049-11","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMyOSwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTExQHlvcG1haWwuY29tIiwiaWF0IjoxNjY2OTgyNTYxLCJleHAiOjE2Njk2NjA5NjF9.Q_huRIJ-Gb25TG9L1Zt-cf-pHMzykOSZqvezv3rF79M"},
+	{"name":"1666982550049-12","email":"1666982550049-12@yopmail.com","password":"1666982550049-12","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMzMCwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTEyQHlvcG1haWwuY29tIiwiaWF0IjoxNjY2OTgyNTYxLCJleHAiOjE2Njk2NjA5NjF9.ZVApPyN3MBaLif148jv86D8A8Qhp__B039fa1C7L3eA"},
+	{"name":"1666982550049-13","email":"1666982550049-13@yopmail.com","password":"1666982550049-13","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMzMSwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTEzQHlvcG1haWwuY29tIiwiaWF0IjoxNjY2OTgyNTYyLCJleHAiOjE2Njk2NjA5NjJ9.1W1s48c3bjWxwJI2tN0mP2enTGJZ6HeaYOjHWv3_bBk"},
+	{"name":"1666982550049-14","email":"1666982550049-14@yopmail.com","password":"1666982550049-14","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMzMiwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTE0QHlvcG1haWwuY29tIiwiaWF0IjoxNjY2OTgyNTYzLCJleHAiOjE2Njk2NjA5NjN9.cb9WpzzFZZaLFco3jbsK82bm1V6bFDvhzyUQmsLF6xk"},
+	{"name":"1666982550049-15","email":"1666982550049-15@yopmail.com","password":"1666982550049-15","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMzMywiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTE1QHlvcG1haWwuY29tIiwiaWF0IjoxNjY2OTgyNTY0LCJleHAiOjE2Njk2NjA5NjR9.IZrXfogUYrDOIN5FV__0fXbqv0MvdMUIcnElLfo58lU"},
+	{"name":"1666982550049-16","email":"1666982550049-16@yopmail.com","password":"1666982550049-16","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMzNCwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTE2QHlvcG1haWwuY29tIiwiaWF0IjoxNjY2OTgyNTY1LCJleHAiOjE2Njk2NjA5NjV9.tV6OiXx_z1GQQvZoqLOqIO0cUzLZg77gvpKElK99exw"},
+	{"name":"1666982550049-17","email":"1666982550049-17@yopmail.com","password":"1666982550049-17","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMzNSwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTE3QHlvcG1haWwuY29tIiwiaWF0IjoxNjY2OTgyNTY2LCJleHAiOjE2Njk2NjA5NjZ9.3VMr4rRWVtjiNjBhePn56_LzK16gWCaac4FFdbVdnWU"},
+	{"name":"1666982550049-18","email":"1666982550049-18@yopmail.com","password":"1666982550049-18","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMzNiwiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTE4QHlvcG1haWwuY29tIiwiaWF0IjoxNjY2OTgyNTY3LCJleHAiOjE2Njk2NjA5Njd9.xZZasp58dpN-PwzlUm6RUHRP6Wf1V--5zBIkgXBNM-Q"},
+	{"name":"1666982550049-19","email":"1666982550049-19@yopmail.com","password":"1666982550049-19","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMzNywiZW1haWwiOiIxNjY2OTgyNTUwMDQ5LTE5QHlvcG1haWwuY29tIiwiaWF0IjoxNjY2OTgyNTY4LCJleHAiOjE2Njk2NjA5Njh9.SSyxogz1YrLqbooCF7hQFcyv0DwopKMwQL1Em8g3Zmw"},
 ];
 
-let getUserId = 5;
+let getUserId = 0;
 
 
 const apiBackend = 'http://development.computatus.com:61976'; // modify this with your backend
@@ -111,3 +121,16 @@ test('loginuserviamail', async () => {
 	await serialEntrepreneurFrontendHandlers.loginUserViaMail(testUsers[userIndex].email, testUsers[userIndex].password, fSuccess, fFail);
 });
 
+test('loginuserviatoken', async () => {
+	let userIndex = getUserId++;
+	tickLog.info(`loginuserviatoken: index ${userIndex} name: ${JSON.stringify(testUsers[userIndex].name)}`);
+	const fSuccess = (props, retval) => {
+		tickLog.info(`fSuccess: ${JSON.stringify(props)} ${JSON.stringify(retval)}`);
+		expect(retval).toMatchObject({"result":"OK"})
+	}
+	const fFail = (props, error) => {
+		tickLog.error(`fFail: ${JSON.stringify(props)} ${JSON.stringify(error)}`);
+		expect(true).toBe(false);
+	}
+	await serialEntrepreneurFrontendHandlers.loginUserViaToken(testUsers[userIndex].token, fSuccess, fFail);
+});
